f39993b2c1f64f0944efe7dd8799fbfd8e90aa3a
Usernow <usernow.82@gmail.com>
2021-05-31 08:44:59 -0500

    cambios nueva linea

--- Exercise6.py
+++ Exercise6.py
@@ -3,3 +3,11 @@ message = "hello world python"
 print(message)
 message = 'hello world from python crush coursw'
 print(message)
+
+
+car = "Renault"
+print("car is == 'Renault', I predict True")
+print(car == "Renault")
+car2 = "Renault"
+print("car is == 'BMW', I predict False")
+print(car == "BMW")
